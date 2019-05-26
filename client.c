d029f42c208edd0be468f92ef36acc866f3d0cce
ErgerSusha <ergersusha2@gmail.com>
2019-05-27 01:35:53 +0300

    Added client implementation

--- client.c
+++ client.c
@@ -2,32 +2,44 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <string.h>
 
-#pragma warning(disable : 4996)
+#pragma warning(disable: 4996)
 
-int main()
-{
+int main() {
     int ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     struct sockaddr_in SockAddr;
     SockAddr.sin_family = AF_INET;
     SockAddr.sin_port = htons(1337);
     SockAddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    connect(ClientSocket, (struct sockaddr *)&SockAddr, sizeof(SockAddr));
+    connect(ClientSocket, (struct sockaddr * ) & SockAddr, sizeof(SockAddr));
 
-    char * msg = malloc(sizeof(char) * 100);
+    char msg[1024];
+    const char usernameMsg[] = "USERNAME MyUName\n";
+    costs char passwordMsg[] = "PASSWORD 12345\n";
 
-    while (msg != "")
-    {
-        scanf("%s", msg);
-        send(ClientSocket, msg, strlen(msg), 0);
-        size_t msg_size = recv(ClientSocket, msg, 1024, 0);
-        msg[msg_size] = 0;
+    send(ClientSocket, & usernameMsg, strlen(usernameMsg), 0); //Send uname
+    recv(ClientSocket, & msg, 1024, 0); // Recv pwd request
+    send(ClientSocket, & passwordMsg, strlen(passwordMsg), 0); //Send pwd 
 
-        printf("%s\n", msg);
+    while (1) {
+        strcpy(msg, "SEND ");
+        scanf("%s", msg + 5); // Destination
+        msg[strlen(msg)] = ' ';
+        scanf("%s", msg + strlen(msg)); // Msg
+        msg[strlen(msg)] = '\n';
+        send(ClientSocket, & msg, strlen(msg), 0); // Send msg
+        recv(ClientSocket, & msg, 1024, 0); // Recv response
+        if (msg[0] == 'M') // Message from user
+        {
+            msg[strstr(msg, " ")] = ':';
+            printf("%s\n", msg);
+        } else // Info message
+        {
+            printf("%s\n", msg + 5);
+        }
     }
 
     return 0;
 }
-
-
